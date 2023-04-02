4bf54ed75746b4c037ce55c0210e2247d631cd00
Netesh5 <netesh.paudel@gmail.com>
2023-04-02 11:10:01 +0545

    server creation

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const {createServer}=require("http");
+const {Server}=require("socket.io");
+
+
+const app=express();
+const httpServer=createServer(app);
+const ioServer=new Server(httpServer);
+
+app.route("/").get((request,response)=>{
+    response.json("Chat app using Socket.io and Flutter");
+})
+ioServer.on("connection",(socket)=>{
+    console.log("Connected");
+    socket.on("sendMsg",(msg)=>{
+        console.log(msg);
+        socket.emit("sendMsgServer",{...msg,type:"other"})
+    })
+    
+});
+
+httpServer.listen(3000);
