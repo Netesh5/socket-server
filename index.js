57156b4581f77f55d8a02312cc06cae50ea03baf
Netesh5 <netesh.paudel@gmail.com>
2023-04-03 13:10:29 +0545

    socket server

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const app=express();
 const httpServer=createServer(app);
 const ioServer=new Server(httpServer);
 
-app.route("/").get((request,response)=>{
-    response.json("Chat app using Socket.io and Flutter");
-})
+// app.route("/").get((request,response)=>{
+//     response.json("Chat app using Socket.io and Flutter");
+// })
 ioServer.on("connection",(socket)=>{
     console.log("Connected");
+    socket.join("group");
     socket.on("sendMsg",(msg)=>{
         console.log(msg);
-        socket.emit("sendMsgServer",{...msg,type:"other"})
+        // socket.emit("sendMsgServer",{...msg,type:"other"})
+        ioServer.to("group").emit("sendMsgServer",{...msg,type:"other"})
     })
     
 });
